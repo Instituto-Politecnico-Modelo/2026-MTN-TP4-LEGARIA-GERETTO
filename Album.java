8dee4737ff43ac4c74dc338363529740f90efdba
Santino Geretto <sangamefan@gmail.com>
2026-04-16 14:24:24 -0300

    hola

---
+++ Album.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Album {
+    private HashSet<Cancion> canciones;
+    private String nombre;
+    private HashSet<Artista> autores;
+}
